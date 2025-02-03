673b84df03b6424547391c00bd0f78ea5e7fb121
John Bissontz <47512933+BissontzJ@users.noreply.github.com>
2025-02-03 08:48:38 -0500

    fixed ESLint <a> element error

--- components/ui/HeaderBanner.tsx
+++ components/ui/HeaderBanner.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 const HeaderBanner = () => {
     return (
         <header id="header-main">
             <h1 id="header-title">
-                <a href="/">
+                <Link href="/">
                     <span id="top-line"> John Bissontz </span>
                     <span id="bottom-line"> Career Day </span>
-                </a>
+                </Link>
             </h1>
         </header>
     )
